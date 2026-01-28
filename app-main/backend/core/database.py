7620618f668f68e3fad00fe8f41cbe0dcf823c67
Pawankumaryada <165542759+Pawankumaryada@users.noreply.github.com>
2026-01-29 00:28:22 +0530

    Fix import path for config in database.py

--- app-main/backend/core/database.py
+++ app-main/backend/core/database.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from core.config import MONGO_URL, DB_NAME
+from backend.core.config import MONGO_URL, DB_NAME
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
