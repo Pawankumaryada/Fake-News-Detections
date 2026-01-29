b4c747b90606b4605b8d143c2a1156b47c03ac04
Pawankumaryada <165542759+Pawankumaryada@users.noreply.github.com>
2026-01-29 21:21:17 +0530

    Update main.py

--- app-main/backend/main.py
+++ app-main/backend/main.py
@@ -9,25 +9,25 @@ from backend.routes.history import router as history_router
 from backend.routes.feedback import router as feedback_router
 from backend.routes.facts import router as facts_router
 
-
 app = FastAPI(
     title="Veritas AI Backend",
     version="1.0.0"
 )
 
-# ---------------- CORS (FIXED) ----------------
+# ✅ CORS (LOCAL + VERCEL)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://fake-news-detections-iota.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ---------------- ROUTES ----------------
+# ROUTES
 app.include_router(analyze_router, prefix="/api")
 app.include_router(auth_router, prefix="/api")
 app.include_router(trending_router, prefix="/api")
@@ -36,7 +36,6 @@ app.include_router(history_router, prefix="/api")
 app.include_router(feedback_router, prefix="/api")
 app.include_router(facts_router, prefix="/api")
 
-# ---------------- HEALTH ----------------
 @app.get("/api/health")
 def health():
     return {"status": "OK"}
