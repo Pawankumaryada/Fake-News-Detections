01725adb89bb0001cff809cd4769c657834321a6
Pawankumaryada <165542759+Pawankumaryada@users.noreply.github.com>
2026-01-31 02:05:24 +0530

    Update index.js

--- app-main/frontend/src/api/index.js
+++ app-main/frontend/src/api/index.js
@@ -4,19 +4,16 @@ const api = axios.create({
   baseURL: "https://fake-news-backend-xom8.onrender.com",
 });
 
-/* TEXT ANALYSIS */
 export async function analyzeText(text) {
   const res = await api.post("/api/analyze/text", { text });
   return res.data;
 }
 
-/* GET ANALYSIS */
 export async function getAnalysisById(id) {
   const res = await api.get(`/api/analyze/${id}`);
   return res.data;
 }
 
-/* TRENDING */
 export async function getTrending() {
   const res = await api.get("/api/trending");
   return res.data;
