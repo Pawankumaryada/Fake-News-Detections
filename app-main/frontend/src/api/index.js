8f082e77ab7a11543f57b75b8b003528ecf6932d
Pawankumaryada <165542759+Pawankumaryada@users.noreply.github.com>
2026-01-31 03:35:12 +0530

    Update index.js

--- app-main/frontend/src/api/index.js
+++ app-main/frontend/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://fake-news-backend-xom8.onrender.com",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://fake-news-backend-xom8.onrender.com",
 });
 
 /* ================= TEXT ANALYSIS ================= */
